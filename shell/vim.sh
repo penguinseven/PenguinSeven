1adb8e80efbdc10e96ebfd3d62f3b72c1e42d4e5
penguinseven <penguinseven@sina.com>
2017-04-06 15:45:51 +0800

    1. update vim setup shell file

--- shell/vim.sh
+++ shell/vim.sh
@@ -21,9 +21,9 @@ mv -f ~/vim ~/.vim
 mv -f ~/.vimrc ~/.vimrc_old
 mv -f ~/.vim/.vimrc ~/
 git clone https://git.coding.net/penguinseven/vundle.git ~/.vim/bundle/vundle
-echo "ma6174正在努力为您安装bundle程序" > ma6174
-echo "安装完毕将自动退出" >> ma6174
-echo "请耐心等待" >> ma6174
-vim ma6174 -c "BundleInstall" -c "q" -c "q"
-rm ma6174
+echo "penguinSeven 正在努力为您安装bundle程序" > penguinSeven 
+echo "安装完毕将自动退出" >> penguinSeven 
+echo "请耐心等待" >> penguinSeven 
+vim penguinSeven  -c "BundleInstall" -c "q" -c "q"
+rm penguinSeven 
 echo "安装完成"
